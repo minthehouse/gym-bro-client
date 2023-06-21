45ebb79f4f773b86ebfb46aaa5449cfc08236895
minthehouse <min.navy.lee@gmail.com>
2023-06-21 14:49:22 -0400

    create initial table to track workout that takes user input of weight/reps.

--- src/app/tab1/track-workout/track-workout.page.ts
+++ src/app/tab1/track-workout/track-workout.page.ts
@@ -1,4 +1,6 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
@@ -6,10 +8,22 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: './track-workout.page.html',
   styleUrls: ['./track-workout.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, FormsModule, CommonModule],
 })
 export class WorkoutTrackPage implements OnInit {
+  workoutList: { set: number; weight: string; reps: string }[] = [
+    { set: 1, weight: '', reps: '' },
+  ];
+
   constructor() {}
 
   ngOnInit() {}
+
+  addRow() {
+    this.workoutList.push({
+      set: this.workoutList.length + 1,
+      weight: '',
+      reps: '',
+    });
+  }
 }
