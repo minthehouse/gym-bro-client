2d13c7a2b706b5b869323992a7baa0229465893f
minthehouse <min.navy.lee@gmail.com>
2023-07-14 18:32:02 -0400

    implement history-workout page further by using formControl

--- src/app/tab1/history-workout/history-workout.page.ts
+++ src/app/tab1/history-workout/history-workout.page.ts
@@ -3,18 +3,25 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonDatetime, IonicModule } from '@ionic/angular';
 import { WorkoutService } from 'src/app/service/workout.service';
-import { MatDatepickerInputEvent, MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatButtonModule } from '@angular/material/button';
+import { filter, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-history-workout',
   templateUrl: './history-workout.page.html',
   styleUrls: ['./history-workout.page.scss'],
   standalone: true,
-  imports: [IonicModule, FormsModule, CommonModule, MatDatepickerModule, MatFormFieldModule, MatInputModule],
+  imports: [
+    IonicModule,
+    FormsModule,
+    CommonModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+  ],
 })
 export class HistoryWorkoutPage implements OnInit {
   @ViewChild(IonDatetime)
@@ -24,25 +31,61 @@ export class HistoryWorkoutPage implements OnInit {
   constructor(private workoutService: WorkoutService) {}
   selectedDate: Date = new Date(); // Set the initial selected date to today's date or any default date
   workoutLists: any = [this.selectedDate];
+  selectedDateControl: FormControl = new FormControl();
+
+  availableDates: Date[] = [];
+  selectedWorkout: any;
+  // Filter function for the Datepicker
+
+  dateFilter = (date: Date): boolean => {
+    // Check if the date is included in the allowed dates array
+    return this.availableDates.some(allowedDate => this.isSameDate(date, allowedDate));
+  };
+
+  isSameDate(date1: Date, date2: Date): boolean {
+    return (
+      date1.getFullYear() === date2.getFullYear() &&
+      date1.getMonth() === date2.getMonth() &&
+      date1.getDate() === date2.getDate()
+    );
+  }
 
   ngOnInit() {
-    this.workoutService.getWorkouts().subscribe(workouts => {
-      console.log('workouts in history', workouts);
-      this.workoutLists = workouts;
+    this.workoutService
+      .getWorkouts()
+      .pipe(
+        filter(workouts => workouts),
+        tap(workouts => {
+          this.selectedWorkout = workouts[workouts.length - 2];
+          this.workoutLists = workouts;
+          this.setAvailableDates(workouts);
+        }),
+      )
+      .subscribe();
+  }
+
+  setAvailableDates(workouts) {
+    workouts.map(workout => {
+      this.availableDates.push(new Date(workout.start_at));
     });
   }
 
-  generateDayValues(): number[] {
-    const dayValues: number[] = [];
-    for (const workout of this.workoutLists) {
-      const startAt = new Date(workout.start_at);
-      const day = startAt.getDate();
-      if (!dayValues.includes(day)) {
-        dayValues.push(day);
-      }
-    }
-    console.log('day values', dayValues);
+  onDateChange(event: any): void {
+    console.log('event in handler', event);
 
-    return dayValues;
+    const selectedDate = event.value; // Retrieve the selected date value
+
+    // Find the workout with the same date as the selected date
+    const selectedWorkout = this.workoutLists.find((workout: any) =>
+      this.isSameDate(new Date(workout.start_at), selectedDate),
+    );
+
+    if (selectedWorkout) {
+      // Do something with the selected workout
+      this.selectedWorkout = selectedWorkout;
+      console.log('Selected workout:', selectedWorkout);
+    } else {
+      console.log('No workout found for the selected date');
+    }
   }
 }
