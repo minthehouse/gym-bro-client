db0abeb74856858878095ed5ceff93f5d57230c0
minthehouse <min.navy.lee@gmail.com>
2023-08-10 18:36:22 -0400

    clean up history-diet page

--- src/app/tab2/history-diet/history-diet.page.ts
+++ src/app/tab2/history-diet/history-diet.page.ts
@@ -2,12 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { WorkoutService } from 'src/app/service/workout.service';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { filter, tap } from 'rxjs/operators';
 import { DietService } from 'src/app/service/diet.service';
+import { DailyCaloriesIntakeComponent } from 'src/app/components/daily-calories-intake/daily-calories-intake.component';
 
 @Component({
   selector: 'app-history-diet',
@@ -22,18 +21,16 @@ import { DietService } from 'src/app/service/diet.service';
     MatFormFieldModule,
     MatInputModule,
     ReactiveFormsModule,
+    DailyCaloriesIntakeComponent,
   ],
 })
 export class HistoryDietPage implements OnInit {
   constructor(private dietService: DietService) {}
-  DietsList: any = [];
   selectedDateControl: FormControl = new FormControl();
   availableDates: Date[] = [];
   selectedDiet: any;
 
-  // Filter function for the Datepicker
   dateFilter = (date: Date): boolean => {
-    // Check if the date is included in the allowed dates array
     return this.availableDates.some(allowedDate => this.isSameDate(date, allowedDate));
   };
 
@@ -47,37 +44,33 @@ export class HistoryDietPage implements OnInit {
 
   ngOnInit() {
     this.dietService.getDiets().subscribe((diets: any) => {
-      this.selectedDiet = diets[diets.length - 1];
-      this.selectedDateControl.setValue(this.selectedDiet.end_at);
-      this.DietsList = diets;
       this.setAvailableDates(diets);
-      console.log('diets', diets);
+      const latestDiet = diets[diets.length - 1];
+      this.selectedDiet = latestDiet;
+      this.selectedDateControl.setValue(latestDiet.created_at);
     });
   }
 
-  setAvailableDates(workouts) {
-    workouts.map(workout => {
-      this.availableDates.push(new Date(workout.end_at));
+  setAvailableDates(diets) {
+    diets.map(diet => {
+      this.availableDates.push(new Date(diet.created_at));
     });
   }
 
   onDateChange(event: any): void {
-    const selectedDate = event.value; // Retrieve the selected date value
+    const selectedDate = event?.value?.toISOString();
 
-    // Find the workout with the same date as the selected date
-    const selectedDiet = this.DietsList.find((workout: any) =>
-      this.isSameDate(new Date(workout.start_at), selectedDate),
-    );
-
-    if (selectedDiet) {
-      // Do something with the selected workout
-      this.selectedDiet = selectedDiet;
-      console.log('Selected workout:', selectedDiet);
-    } else {
-      console.log('No workout found for the selected date');
+    if (selectedDate) {
+      this.getDietByDate(selectedDate);
     }
   }
-  navigateToPreviousWorkout() {}
 
-  navigateToNextWorkout() {}
+  private getDietByDate(dateInISOString) {
+    this.dietService.getDietByDate(dateInISOString).subscribe((diet: any) => {
+      if (diet) {
+        this.selectedDiet = diet;
+        this.selectedDateControl.setValue(diet.created_at);
+      }
+    });
+  }
 }
