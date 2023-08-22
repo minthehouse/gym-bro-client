b0a164da1ba4cab86d7f699c9c35659855b36e94
minthehouse <min.navy.lee@gmail.com>
2023-08-22 19:29:45 -0400

    edit goal page

--- src/app/auth/goal/goal.page.ts
+++ src/app/auth/goal/goal.page.ts
@@ -3,11 +3,11 @@ import { UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms
 import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, ToastController } from '@ionic/angular';
 import { AuthService } from 'src/app/service/auth/auth.service';
 import { UserService } from 'src/app/service/user.service';
 import { Select, Store } from '@ngxs/store';
-import { Observable, switchMap } from 'rxjs';
+import { Observable, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-goal',
@@ -34,6 +34,7 @@ export class GoalPage implements OnInit {
     private store: Store,
     private router: Router,
     public authService: AuthService,
+    private toastController: ToastController,
   ) {}
 
   ngOnInit(): void {
@@ -42,7 +43,8 @@ export class GoalPage implements OnInit {
         this.form.patchValue(user);
       }
     });
-    this.isEditMode = this.router.getCurrentNavigation().extras?.state?.isEditMode;
+    this.isEditMode =
+      this.router.getCurrentNavigation().extras?.state?.isEditMode || this.router.url.includes('update-profile');
     this.creds = this.router.getCurrentNavigation().extras.state?.cred;
   }
 
@@ -51,8 +53,27 @@ export class GoalPage implements OnInit {
     if (ngf.valid) {
       this.userService
         .update(user.id, ngf.form.value)
-        .pipe(switchMap(() => this.authService.login(this.creds)))
+        .pipe(
+          switchMap(() => {
+            if (!this.isEditMode) {
+              return this.authService.login(this.creds);
+            } else {
+              return this.presentToast();
+            }
+          }),
+        )
         .subscribe();
     }
+  }
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Your Goal is successfully updated!',
+      duration: 3000,
+      position: 'bottom',
+      color: 'success',
+    });
+
+    await toast.present();
   }
 }
