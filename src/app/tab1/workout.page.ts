fbf66266e4dcf101e1a83d47981f9375639ed788
minthehouse <min.navy.lee@gmail.com>
2023-11-14 19:17:21 -0500

    make text dynamic by using ngIf in workout page

--- src/app/tab1/workout.page.ts
+++ src/app/tab1/workout.page.ts
@@ -2,15 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { WorkoutService } from '../service/workout.service';
+import { Observable } from 'rxjs';
+import { Select } from '@ngxs/store';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-workout',
   templateUrl: 'workout.page.html',
   styleUrls: ['workout.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule],
 })
 export class WorkoutPage implements OnInit {
+  @Select(state => state.workouts.current) currentWorkout$: Observable<any>;
+
   constructor(private router: Router, private route: ActivatedRoute, private workoutService: WorkoutService) {}
 
   ngOnInit(): void {
