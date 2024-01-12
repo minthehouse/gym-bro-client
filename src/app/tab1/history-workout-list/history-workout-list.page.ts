da224b080f7714f81854a0953a8d25ca08d30f54
minthehouse <min.navy.lee@gmail.com>
2024-01-11 19:01:10 -0500

    update style on history workout list page and add backBtn component

--- src/app/tab1/history-workout-list/history-workout-list.page.ts
+++ src/app/tab1/history-workout-list/history-workout-list.page.ts
@@ -4,31 +4,23 @@ import { Observable } from 'rxjs';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { BackBtnComponent } from 'src/app/components/back-button/back-button.component';
 
 @Component({
   selector: 'app-history-workout-list',
   templateUrl: './history-workout-list.page.html',
   styleUrls: ['./history-workout-list.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule],
+  imports: [IonicModule, CommonModule, BackBtnComponent],
 })
 export class HistoryWorkoutListPage implements OnInit {
-  @Select(state => state.workouts.list) pastWorkouts$: Observable<any>;
+  @Select(state => state.workouts.list) workoutList$: Observable<any>;
 
   constructor(private router: Router) {}
 
-  ngOnInit() {
-    this.pastWorkouts$.subscribe(pw => {
-      console.log('pw', pw);
+  ngOnInit() {}
 
-      // in this page, I need to display the Date for each workout.
-      // can probably display duration using start_at, end_at?
-      // can probably keep the calendar to show how many times user worked out.
-    });
-  }
-
-  onClick(workout) {
-    console.log('workout on onClick', workout);
-    this.router.navigate([`tabs/workout/${workout.id}/details`]);
+  onClick(selectedWorkout) {
+    this.router.navigate([`tabs/workout/${selectedWorkout.id}/details`]);
   }
 }
