69d3fcee31a8cb7a3c543ddeb8c0ddcd68899449
minthehouse <min.navy.lee@gmail.com>
2024-01-18 17:51:27 -0500

    build workoutLoad from history workout page and pass down to progressive workout comp

--- src/app/components/progressive-workout/progressive-workout.component.ts
+++ src/app/components/progressive-workout/progressive-workout.component.ts
@@ -1,60 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Component, Input } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
-import { WorkoutService } from 'src/app/service/workout.service';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-progressive-workout',
   templateUrl: './progressive-workout.component.html',
   styleUrls: ['./progressive-workout.component.scss'],
   standalone: true,
-  imports: [
-    IonicModule,
-    FormsModule,
-    CommonModule,
-    MatDatepickerModule,
-    MatFormFieldModule,
-    MatInputModule,
-    ReactiveFormsModule,
-    MatNativeDateModule,
-    MatButtonModule,
-  ],
+  imports: [IonicModule, CommonModule],
 })
-export class ProgresssiveWorkoutComponent implements OnInit {
+export class ProgresssiveWorkoutComponent {
   @Input() workout: any;
-
-  constructor(private workoutService: WorkoutService) {}
-
-  workoutLists: any = [];
-  range = new FormGroup({
-    start: new FormControl<Date | null>(null),
-    end: new FormControl<Date | null>(null),
-  });
-  availableDates: Date[] = [];
-  selectedWorkout: any;
-
-  ngOnInit() {
-    this.setDefaultRange();
-  }
-
-  private setDefaultRange() {
-    const endDate = new Date();
-    const startDate = new Date();
-    startDate.setDate(endDate.getDate() - 6); // Set it to a week ago
-
-    this.range.setValue({
-      start: startDate,
-      end: endDate,
-    });
-  }
-
-  changeRange() {
-    console.log('hit apply');
-  }
 }
--- src/app/tab1/history-workout/history-workout.page.ts
+++ src/app/tab1/history-workout/history-workout.page.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
-import { Select } from '@ngxs/store';
 import { BackBtnComponent } from 'src/app/components/back-button/back-button.component';
 import { DailyWorkoutComponent } from 'src/app/components/daily-workout/daily-workout.component';
 import { ProgresssiveWorkoutComponent } from 'src/app/components/progressive-workout/progressive-workout.component';
@@ -24,21 +23,69 @@ import { TrackWorkoutPage } from '../track-workout/track-workout.page';
   ],
 })
 export class HistoryWorkoutPage implements OnInit {
-  @Select(state => state.workouts.list) workouts$;
   constructor(private workoutService: WorkoutService, private activatedRoute: ActivatedRoute) {}
   currentSegment: string = 'daily';
-  workout: any;
+  workoutLoad: any;
   ngOnInit() {
     const params = this.activatedRoute.snapshot.paramMap;
     const workoutId = params.get('id');
 
     this.workoutService.getById(workoutId).subscribe(workout => {
-      this.workout = workout;
+      console.log('workout in history', workout);
+      this.workoutLoad = this.getWorkloadDetails(workout.exercises);
+      console.log('hi', this.workoutLoad);
     });
   }
 
   segmentChanged(ev: any) {
     console.log('Segment changed', ev);
     this.currentSegment = ev.detail.value;
+  }
+
+  getWorkloadDetails(exercises: any[]) {
+    return {
+      totalWorkload: this.calculateTotalWorkload(exercises),
+      maxWeightPerExercise: this.findHighestWeight(exercises),
+    };
+  }
+
+  calculateTotalWorkload(exercises: any[]): { name: string; value: number }[] {
+    const resultArray: { name: string; value: number }[] = [];
+
+    exercises.forEach(exercise => {
+      const exerciseName = exercise.ExerciseType.name;
+      const volume = exercise.rep * exercise.weight;
+
+      // Update or add entry in resultArray
+      const existingEntry = resultArray.find(entry => entry.name === exerciseName);
+      if (existingEntry) {
+        existingEntry.value += volume;
+      } else {
+        resultArray.push({ name: exerciseName, value: volume });
+      }
+    });
+
+    // Calculate total workload for the "Total" entry
+    const totalValue = resultArray.reduce((sum, entry) => sum + entry.value, 0);
+    resultArray.push({ name: 'Total', value: totalValue });
+
+    return resultArray;
+  }
+
+  findHighestWeight(exercises: any[]): { name: string; value: number }[] {
+    return exercises.reduce((resultArray, exercise) => {
+      const exerciseName = exercise.ExerciseType.name;
+      const weight = exercise.weight;
+
+      const existingEntry = resultArray.find(entry => entry.name === exerciseName);
+
+      if (!existingEntry) {
+        resultArray.push({ name: exerciseName, value: weight });
+      } else if (weight > existingEntry.value) {
+        existingEntry.value = weight;
+      }
+
+      return resultArray;
+    }, []);
   }
 }
