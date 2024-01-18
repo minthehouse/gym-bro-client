d0e61ee3da7f8e888119ed2d5837959a61e8d07a
minthehouse <min.navy.lee@gmail.com>
2024-01-17 19:28:51 -0500

    remove unnecessary observable from workout page

--- src/app/tab1/workout.page.ts
+++ src/app/tab1/workout.page.ts
@@ -1,9 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
-import { WorkoutService } from '../service/workout.service';
-import { Observable } from 'rxjs';
-import { Select } from '@ngxs/store';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -13,14 +10,8 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [IonicModule, CommonModule],
 })
-export class WorkoutPage implements OnInit {
-  @Select(state => state.workouts.current) currentWorkout$: Observable<any>;
-
-  constructor(private router: Router, private route: ActivatedRoute, private workoutService: WorkoutService) {}
-
-  ngOnInit(): void {
-    this.workoutService.getWorkouts().subscribe();
-  }
+export class WorkoutPage {
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
   sendEvent(url: string) {
     this.router.navigate([url], { relativeTo: this.route });
